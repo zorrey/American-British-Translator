fdbca857fda429865457e3980160d5a4d2679d14
Zorrey <102656748+zorrey@users.noreply.github.com>
2022-08-19 21:48:19 +0300

    begin routs

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,16 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      let {text , locale} = req.body;
+      if(!locale || text == undefined) return res.json({error: 'Required field(s) missing'});
+      if(text == "") return res.json({error: 'No text to translate'});
+      if(locale == 'american-to-british'){
+
+      }
+      else if(locale == 'british-to-american'){
+
+      }
+      else return res.json({error: 'Invalid value for locale field'});
       
     });
 };
